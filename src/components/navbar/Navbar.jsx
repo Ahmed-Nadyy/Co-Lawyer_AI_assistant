43278accc5873d08fb97088c8819e9ebe566942c
Ahmed Nady <120312236+NAADDY@users.noreply.github.com>
2024-06-28 02:07:59 +0300

    a

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,7 @@ const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
     <p><a href='#wgpt3'>About Co-Lawyer?</a></p>
-    {/* <p><a href='#possibility'>Open.AI</a></p> */}
     <p><a href='#features'>Case Studies</a></p>
-    {/* <p><a href='#blog'>Library</a></p> */}
   </>
 )
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -9,17 +9,7 @@ const Blog = () => {
         Stay Updated: Discover What's New on Our Blog!
         </h1>
       </div>
-      {/* <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container_groupA">
-          <Article imgUrl={blog01} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        </div>
-        <div className="gpt3__blog-container_groupB">
-          <Article imgUrl={blog02} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-          <Article imgUrl={blog03} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-          <Article imgUrl={blog04} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-          <Article imgUrl={blog05} date="Jul 05,2022" title="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        </div>
-      </div> */}
+      
     </div>
   )
 }
